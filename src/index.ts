e3275084ea1a80315a3833e8515068dec50ed003
rutgerswirski <rutgerswirski.johan@gmail.com>
2026-05-01 12:33:28 +0200

    fix: uncomment Button export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// export { Button } from "./components/Button";
+export { Button } from "./components/Button";
 // export { SectionLabel } from "./components/SectionLabel";
 // export { DotGrid } from "./components/DotGrid";
 // export { ComicPanel } from "./components/ComicPanel";
