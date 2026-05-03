933bc77c4e5d4efa6153b2ab555f15488f90e32f
rutgerswirski <rutgerswirski.johan@gmail.com>
2026-05-03 16:09:14 +0200

    fix: add cursor pointer style to Button component for improved user interaction

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 // src/components/button/Button.tsx
 import clsx from "clsx";
 import { jsx } from "react/jsx-runtime";
-var baseStyles = "inline-block border-4 border-black px-6 py-3 font-black uppercase transition-all duration-200 shadow-[6px_6px_0_#0B0B0C] hover:translate-x-1 hover:translate-y-1 hover:shadow-none active:translate-x-2 active:translate-y-2";
+var baseStyles = "inline-block hover:cursor-pointer border-4 border-black px-6 py-3 font-black uppercase transition-all duration-200 shadow-[6px_6px_0_#0B0B0C] hover:translate-x-1 hover:translate-y-1 hover:shadow-none active:translate-x-2 active:translate-y-2";
 var variants = {
   blue: "bg-[#0057FF] text-white",
   yellow: "bg-[#FFD400] text-black",
