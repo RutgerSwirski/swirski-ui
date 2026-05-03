6d11cb5029698e32d051567e3c89878107c67400
rutgerswirski <rutgerswirski.johan@gmail.com>
2026-05-03 11:02:24 +0200

    feat: enhance CardContent component to accept className prop for better styling flexibility

--- src/components/card/CardContent.tsx
+++ src/components/card/CardContent.tsx
@@ -1,6 +1,12 @@
 // CardContent.tsx
 import { ReactNode } from "react";
 
-export function CardContent({ children }: { children: ReactNode }) {
-  return <div className="p-5">{children}</div>;
+export function CardContent({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return <div className={`p-5 ${className}`}>{children}</div>;
 }
